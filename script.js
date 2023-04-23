63b0fc5bc1419af7e1da28f5f90d1ef40f4388c6
Axel Robles <axelarep@gmail.com>
2023-04-23 14:50:03 -0600

    Local Storage test implementation, not defined yet

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let ulEl = document.getElementById('ul-el');
 
 let userLinks = [];
 
-//LocalStorage
+
 
 function pushLink() {
     userLinks.push(inputEl.value);
@@ -15,11 +15,22 @@ function pushLink() {
 
     //input to empty
     inputEl.value = ''
+
+    //LocalStorage
+    //Save userLinks array to localStorage
+    userLinks = JSON.stringify(userLinks)
+
+    //Save userLinks to localStorage
+    localStorage.setItem('userLinks', userLinks)
+
+    //Convert userLinks to array
+    userLinks = JSON.parse(userLinks)
 }
 
 function displayLink() {
     let linkToDisplay = '';
     for (i=0 ; i<userLinks.length ; i++) {
+
 
         //assign list HTML element to variable
         linkToDisplay += `<li><a href='${userLinks[i]}' target='_blank'>${userLinks[i]}</a></li>`
